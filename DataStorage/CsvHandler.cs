cbd2f19f608924ab08100e8d28ec2f9c762f2a49
hellrookie <hellrookie@163.com>
2019-06-13 16:56:30 +0100

    complete repository
    fix solution issues.
    add data storage tools

---
+++ DataStorage/CsvHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Hellrookie.ToolKit
+{
+    public class CsvHandler
+    {
+        public static void WriteToFile<T>(string path, IEnumerable<T> datas)
+        {
+            path = path.EndsWith(".csv") ? path : path + ".csv";
+            var props = typeof(T).GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Instance);
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                sw.WriteLine(GetHeaderString(props));
+                foreach(var d in datas)
+                {
+                    sw.WriteLine(GetValueString(props, d));
+                }
+            }
+        }
+
+        public static List<T> ReadFromFile<T>(string path)
+        {
+            var result = new List<T>();
+            using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+            {
+                var props = GetPropertyNames(sr.ReadLine());
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var values = line.Split(',');
+                    result.Add(SetValueToObj<T>(props, values.Select(v => { return v.Trim('\"'); }).ToList()));
+                }
+            }
+            return result;
+        }
+
+        private static T SetValueToObj<T>(List<string> propNames, List<string> values)
+        {
+            T obj = Activator.CreateInstance<T>();
+            Dictionary<string, PropertyInfo> objPorps = (typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty | BindingFlags.GetProperty)).ToDictionary(p => p.Name, p=>p);
+            for(int i = 0; i < propNames.Count(); ++i)
+            {
+                if(objPorps.ContainsKey(propNames[i]))
+                {
+                    objPorps[propNames[i]].SetValue(obj, values[i], null);
+                }
+            }
+            return obj;
+        }
+
+        private static List<string> GetPropertyNames(string headerLine)
+        {
+            return new List<string>(headerLine.Split(',').Select(h => { return h.Trim('\"'); }));
+        }
+
+        private static string GetHeaderString(IEnumerable<PropertyInfo> props)
+        {
+            StringBuilder header = new StringBuilder();
+            foreach(var p in props)
+            {
+                header.AppendFormat("{0},", p.Name);
+            }
+            return header.ToString().TrimEnd(',');
+        }
+
+        private static string GetValueString<T>(IEnumerable<PropertyInfo> props, T data)
+        {
+            StringBuilder value = new StringBuilder();
+            foreach (var p in props)
+            {
+                var tempValue = p.GetValue(data, null).ToString();
+                if(tempValue.Contains(","))
+                {
+                    tempValue = string.Format("\"{0}\"", tempValue);
+                }
+                value.AppendFormat("{0},", tempValue);
+            }
+            return value.ToString().TrimEnd(',');
+        }
+    }
+}
---
+++ DataStorage/ExcelHandler.cs
@@ -0,0 +1,229 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Hellrookie.ToolKit
+{
+    public class ExcelHandler
+    {
+        public static Dictionary<string, List<Dictionary<string, Object>>> ReadFromFile(string path)
+        {
+            Dictionary<string, List<Dictionary<string, Object>>> data = new Dictionary<string, List<Dictionary<string, Object>>>();
+            IWorkbook wk = GetWorkbook(path);
+            if (wk != null)
+            {
+                for (int i = 0; i < wk.NumberOfSheets; ++i)
+                {
+                    var sheet = wk.GetSheetAt(i);
+                    List<Dictionary<string, Object>> sheetData = GetSheetData(sheet);
+                    data.Add(sheet.SheetName, sheetData);
+                }
+            }
+            return data;
+        }
+
+        public static List<T> ReadFromSheet<T>(string path, int sheetPosition)
+        {
+            IWorkbook wk = GetWorkbook(path);
+            return ReadFromSheet<T>(wk.GetSheetAt(sheetPosition));
+        }
+
+        public static void WriteToFile<T>(string path, List<T> data)
+        {
+            path = path.EndsWith(".xls") ? path : path + "-" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".xls";
+
+            var props = typeof(T).GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Instance);
+
+            HSSFWorkbook workbook = new HSSFWorkbook(); 
+            ISheet sheet = workbook.CreateSheet(Path.GetFileNameWithoutExtension(path));
+            IRow headRow = sheet.CreateRow(0);
+            for (int i = 0; i < props.Length; i++)
+            {
+                headRow.CreateCell(i).SetCellValue(props[i].Name);
+            }
+
+            int rowPos = 1;
+            foreach(var d in data)
+            {
+                var row = sheet.CreateRow(rowPos++);
+                for (int i = 0; i < props.Length; i++)
+                {
+                    SetCellValue(row.CreateCell(i), props[i].GetValue(d, null));
+                }
+            }
+
+            using (FileStream file = new FileStream(path, FileMode.Create))
+            {
+                workbook.Write(file);
+            }
+        }
+
+        private static IWorkbook GetWorkbook(string path)
+        {
+            string extension = System.IO.Path.GetExtension(path);
+            using (FileStream fs = File.OpenRead(path))
+            {
+                if (extension.Equals(".xls"))
+                {
+                    //把xls文件中的数据写入wk中
+                    return new HSSFWorkbook(fs);
+                }
+                else if (extension.Equals(".xlsx"))
+                {
+                    //把xlsx文件中的数据写入wk中
+                    return new XSSFWorkbook(fs);
+                }
+            }
+            return null;
+        }
+
+        private static List<T> ReadFromSheet<T>(ISheet sheet)
+        {
+            List<T> formattedData = new List<T>();
+            List<Dictionary<string, object>> sheetData = GetSheetData(sheet, true);
+            foreach(var d in sheetData)
+            {
+                T data = System.Activator.CreateInstance<T>();
+                foreach (var prop in data.GetType().GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.SetProperty | System.Reflection.BindingFlags.GetProperty))
+                {
+                    if(d.ContainsKey(prop.Name.ToLower()))
+                    {
+                        if (d[prop.Name.ToLower()].GetType().Equals(prop.PropertyType))
+                        {
+                            prop.SetValue(data, d[prop.Name.ToLower()], null);
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Format("The type for the values are not match, data name: {0}, type need: {1}, type in file: {2}", prop.Name, prop.PropertyType, d[prop.Name].GetType()));
+                            continue;
+                        }
+                    }
+                }
+                formattedData.Add(data);
+            }
+            return formattedData;
+        }
+
+        private static List<Dictionary<string, Object>> GetSheetData(ISheet sheet, bool needRemoveSpaceFromHead = false)
+        {
+            var head = GetHead(sheet.GetRow(0));
+            List<Dictionary<string, Object>> sheetData = new List<Dictionary<string, object>>();
+            for (int j = 1; j <= sheet.LastRowNum; ++j)
+            {
+                sheetData.Add(GetRowData(sheet.GetRow(j), head, needRemoveSpaceFromHead));
+            }
+            return sheetData;
+        }
+
+        private static Dictionary<string, object> GetRowData(IRow row, string[] head, bool needRemoveSpaceFromHead)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            for(int i = 0; i < row.LastCellNum; ++i)
+            {
+                string h = string.Empty;
+                if(head.Length > i)
+                {
+                    h = head[i];
+                }
+                else
+                {
+                    h = string.Format("OutOfRangeHead at {0}", i);
+                }
+                data.Add((needRemoveSpaceFromHead ? h.Replace(" ", "") : h).ToLower(), GetCellValue(row.GetCell(i)));
+            }
+            return data;
+        }
+
+        private static string[] GetHead(IRow row)
+        {
+            string[] head = new string[row.LastCellNum];
+            for(int i = 0; i < row.LastCellNum; ++i)
+            {
+                head[i] = row.GetCell(i).ToString();
+            }
+            return head;
+        }
+
+        private static object GetCellValue(ICell cell)
+        {
+            object value = null;
+            try
+            {
+                if (cell.CellType != CellType.Blank)
+                {
+                    switch (cell.CellType)
+                    {
+                        case CellType.Numeric:
+                            // Date comes here
+                            if (DateUtil.IsCellDateFormatted(cell))
+                            {
+                                value = cell.DateCellValue;
+                            }
+                            else
+                            {
+                                // Numeric type
+                                value = cell.NumericCellValue;
+                            }
+                            break;
+                        case CellType.Boolean:
+                            // Boolean type
+                            value = cell.BooleanCellValue;
+                            break;
+                        case CellType.Formula:
+                            value = cell.CellFormula;
+                            break;
+                        default:
+                            // String type
+                            value = cell.StringCellValue;
+                            break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                value = "";
+            }
+
+            return value;
+        }
+
+        private static void SetCellValue(ICell cell, object obj)
+        {
+            if(obj == null)
+            {
+                cell.SetCellValue(string.Empty);
+            }
+            else if (obj.GetType() == typeof(int))
+            {
+                cell.SetCellValue((int)obj);
+            }
+            else if (obj.GetType() == typeof(double))
+            {
+                cell.SetCellValue((double)obj);
+            }
+            else if (obj.GetType() == typeof(IRichTextString))
+            {
+                cell.SetCellValue((IRichTextString)obj);
+            }
+            else if (obj.GetType() == typeof(string))
+            {
+                cell.SetCellValue(obj.ToString());
+            }
+            else if (obj.GetType() == typeof(DateTime))
+            {
+                cell.SetCellValue((DateTime)obj);
+            }
+            else if (obj.GetType() == typeof(bool))
+            {
+                cell.SetCellValue((bool)obj);
+            }
+            else
+            {
+                cell.SetCellValue(obj.ToString());
+            }
+        }
+    }
+}
